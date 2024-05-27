725086675cbd2b25a38d4bed801d2b17d5d6f506
MaiiCol07 <maicolmoreno.dev@hotmail.com>
2024-05-27 01:18:18 -0500

    Terminación de detalles de "Sobre Mi"; "Cualidades" terminado.

--- js/index.js
+++ js/index.js
@@ -3,34 +3,34 @@ document.addEventListener("DOMContentLoaded", function () {
 })
 function cambioStroke(){
     if(document.getElementById("sobreMi").style.display === "block"){
-        document.getElementById("SVGsobreMi").style.stroke = "#9FF5E8"
+        document.getElementById("SVGsobreMi").style.stroke = "beige"
     }else{
         document.getElementById("SVGsobreMi").style.stroke = "#000000"
     }
-    if(document.getElementById("habilidades").style.display === "block"){
-        document.getElementById("SVGhabilidades").style.stroke =
-            "#9FF5E8"
+    if(document.getElementById("cualidades").style.display === "block"){
+        document.getElementById("SVGcualidades").style.stroke =
+            "beige"
     }else{
-        document.getElementById("SVGhabilidades").style.stroke =
+        document.getElementById("SVGcualidades").style.stroke =
             "#000000"
     }
     if(document.getElementById("conocimientos").style.display === "block"){
         document.getElementById("SVGconocimientos").style.stroke =
-            "#9FF5E8"
+            "beige"
     }else{
         document.getElementById("SVGconocimientos").style.stroke =
             "#000000"
     }
     if(document.getElementById("proyectos").style.display === "block"){
         document.getElementById("SVGproyectos").style.stroke =
-            "#9FF5E8"
+            "beige"
     }else{
         document.getElementById("SVGproyectos").style.stroke =
             "#000000"
     }
     if(document.getElementById("contacto").style.display === "block"){
         document.getElementById("SVGcontacto").style.stroke =
-            "#9FF5E8"
+            "beige"
     }else{
         document.getElementById("SVGcontacto").style.stroke = "#000000";
     }
@@ -40,17 +40,17 @@ function cambioStroke(){
 function sobreMi() {
     console.log("Sobre Mi Funcionando Correctamente")
     document.getElementById("sobreMi").style.display = "block"
-    document.getElementById("habilidades").style.display = "none"
+    document.getElementById("cualidades").style.display = "none"
     document.getElementById("conocimientos").style.display = "none"
     document.getElementById("proyectos").style.display = "none"
     document.getElementById("contacto").style.display = "none"
     cambioStroke()
 }
 
-function habilidades() {
-    console.log("habilidades Funcionando Correctamente")
+function cualidades() {
+    console.log("cualidades Funcionando Correctamente")
     document.getElementById("sobreMi").style.display = "none"
-    document.getElementById("habilidades").style.display = "block"
+    document.getElementById("cualidades").style.display = "block"
     document.getElementById("conocimientos").style.display = "none"
     document.getElementById("proyectos").style.display = "none"
     document.getElementById("contacto").style.display = "none"
@@ -60,7 +60,7 @@ function habilidades() {
 function conocimientos() {
     console.log("Conocimiento Funcionando Correctamente")
     document.getElementById("sobreMi").style.display = "none"
-    document.getElementById("habilidades").style.display = "none"
+    document.getElementById("cualidades").style.display = "none"
     document.getElementById("conocimientos").style.display = "block"
     document.getElementById("proyectos").style.display = "none"
     document.getElementById("contacto").style.display = "none"
@@ -70,7 +70,7 @@ function conocimientos() {
 function proyectos() {
     console.log("Proyectos Funcionando Correctamente")
     document.getElementById("sobreMi").style.display = "none"
-    document.getElementById("habilidades").style.display = "none"
+    document.getElementById("cualidades").style.display = "none"
     document.getElementById("conocimientos").style.display = "none"
     document.getElementById("proyectos").style.display = "block"
     document.getElementById("contacto").style.display = "none"
@@ -80,7 +80,7 @@ function proyectos() {
 function contacto() {
     console.log("Contacto Funcionando Correctamente")
     document.getElementById("sobreMi").style.display = "none"
-    document.getElementById("habilidades").style.display = "none"
+    document.getElementById("cualidades").style.display = "none"
     document.getElementById("conocimientos").style.display = "none"
     document.getElementById("proyectos").style.display = "none"
     document.getElementById("contacto").style.display = "block"
